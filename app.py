8c17f0da7bae60f35d83aa07bb160864f58f7252
Czerny <mainist@naver.com>
2025-02-21 00:07:25 +0900

    Revert "save_message 중복 호출 제거"
    This reverts commit 743a1bc4ea2948201c23f03201ce36416a374c32.

--- app.py
+++ app.py
@@ -161,6 +161,7 @@ if file and openai_api_key:
         with st.chat_message("ai"):
             response = get_response(message, st.session_state.retriever)
             st.markdown(response.content)
+            save_message(response.content, "ai")
 
 elif not openai_api_key:
     st.warning("사이드바에 OpenAI API 키를 입력해주세요")
