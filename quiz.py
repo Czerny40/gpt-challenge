1110431ead342ef4441dc0a754e559ef69ebfda3
Czerny <mainist@naver.com>
2025-02-24 02:45:27 +0900

    1

--- quiz.py
+++ quiz.py
@@ -2,7 +2,7 @@ import json
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import UnstructuredFileLoader
-from langchain.text_splitters import CharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter
 from langchain.prompts import ChatPromptTemplate
 from langchain.retrievers import WikipediaRetriever
 from langchain.callbacks import StreamingStdOutCallbackHandler
